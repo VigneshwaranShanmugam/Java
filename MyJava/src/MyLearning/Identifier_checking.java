f269e0705d1a61fa09d9198ae901da296432a781
VigneshwaranShanmugam <vkeyvignesh53@gmail.com>
2018-01-08 18:53:17 +0530

    New commit

---
+++ MyJava/src/MyLearning/Identifier_checking.java
@@ -0,0 +1,17 @@
+package MyLearning;
+
+public class Identifier_checking { 
+//	Here Identifier_checking is the identifier which species the class name
+public static void main(String arg[]) {
+	// b, s, i is the identifiers which specifies variable name
+	byte b=127;
+	short s=32767;
+	int i=234;
+    int _$=22;
+   // int 12p=23;
+    int I=234;
+    //int if=21;
+    
+	
+}
+}
